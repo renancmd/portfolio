6e97ce69a20e1f1addeb319fe7eb52ce8129eb73
Renan <renanmendes421@icloud.com>
2025-05-28 10:43:11 -0300

    feat: curriculum download

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -45,8 +45,10 @@ const HeroSection = () => {
               Contate-me
             </Link>
             <Link
-              href="/"
+              href="Currículo (Virtual).pdf"
+              target="_blank"
               className="px-1 inline-block py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-primary-500 to-secondary-500 hover:bg-slate-800 text-white mt-3"
+              download
             >
               <span className="block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2">
                 Download Currículo
